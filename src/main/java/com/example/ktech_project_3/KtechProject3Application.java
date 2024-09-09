8062837cfe15bd5e920849f88b1f005061d509da
Dung Thùy <dungkiu476@gmail.com>
2024-09-09 10:21:16 +0900

    start

---
+++ src/main/java/com/example/ktech_project_3/KtechProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.ktech_project_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtechProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtechProject3Application.class, args);
+	}
+
+}
